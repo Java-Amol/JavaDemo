f5746c09ff92d43584f3639922ed4e823a9c9339
amolrajput <maheramol00@gmail.com>
2018-12-05 13:24:01 +0530

    First commit

---
+++ src/com/java/scp/exception/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package com.java.scp.exception;
+
+public class ExceptionHandling {
+		
+	public static void main(String[] args) {
+		
+		try {
+			System.out.println(10/0);
+		} catch (Exception e) {
+			
+			System.out.println("error");
+		}
+		
+	}
+}
